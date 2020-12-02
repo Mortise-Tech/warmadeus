41eeb5ffa763c614c28586b83ec32ad9ceb8b8c4
jarodriguez045 <5765093-jarodriguez045@users.noreply.gitlab.com>
2020-12-02 17:24:12 -0500

    add auth routing

--- warmade_site/app.py
+++ warmade_site/app.py
@@ -1,6 +1,9 @@
 from flask import Flask
+
 from home.views import home_bp
+from auth.views import auth_bp
 
 app = Flask(__name__)
 
 app.register_blueprint(home_bp)
+app.register_blueprint(auth_bp)
---
+++ warmade_site/auth/views.py
@@ -0,0 +1,69 @@
+import functools
+
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from warmade_site.models import veteran_db
+
+auth_bp = Blueprint(
+    'auth',
+    url_prefix='/auth',
+    static_folder='static',
+    template_folder='templates'
+    )
+
+@auth_bp.route('/register', methods=('GET', 'POST'))
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = veteran_db()
+        error = None
+
+        if not username:
+            error = 'Username is required.'
+        elif not password:
+            error = 'Password is required.'
+        elif db.execute(
+            'SELECT id FROM user WHERE username = ?', (username,)
+        ).fetchone() is not None:
+            error = 'User {} is already registered.'.format(username)
+
+        if error is None:
+            db.execute(
+                'INSERT INTO user (username, password) VALUES (?, ?)',
+                (username, generate_password_hash(password))
+            )
+            db.commit()
+            return redirect(url_for('auth.login'))
+
+        flash(error)
+
+    return render_template('register.html')
+
+@auth_bp.route('/login', methods=('GET', 'POST'))
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = veteran_db()
+        error = None
+        user = db.execute(
+            'SELECT * FROM user WHERE username = ?', (username,)
+        ).fetchone()
+
+        if user is None:
+            error = 'Incorrect username.'
+        elif not check_password_hash(user['password'], password):
+            error = 'Incorrect password.'
+
+        if error is None:
+            session.clear()
+            session['user_id'] = user['id']
+            return redirect(url_for('index'))
+
+        flash(error)
+
+    return render_template('login.html')
