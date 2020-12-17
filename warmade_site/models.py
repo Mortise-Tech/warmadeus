41663c38f8e8b4aa49f629f2da3df208355a1e0e
Jose Rodriguez <jarodriguez045@gmail.com>
2020-12-16 20:20:27 -0500

    update models

--- warmade_site/models.py
+++ warmade_site/models.py
@@ -1,4 +1,5 @@
-from . import app
+#warmade_site/model.py
+from . import init_app as app
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy(app)
@@ -8,10 +9,10 @@ db.session.commit()
 """
 
 class Product(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
+    id =db.Column(db.Integer, primary_key = True)
     product_name = db.Column(db.String(50), index=True)
 
-class Veteran(db.Model):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), index=True, unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
